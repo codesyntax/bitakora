4a1a69e81cbaa20daeed1ae5c1f5a5ff3bfcaf36
csmant <csmant@anboto>
2016-03-15 15:50:34 +0100

    RSS redirect set

--- bitakora/base/urls.py
+++ bitakora/base/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url, include
 from bitakora.rss.feeds import BlogEntriesFeed
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('bitakora.base.views',
     url(r'^new-blog$', 'new_blog', name='new_blog'),
@@ -9,7 +10,9 @@ urlpatterns = patterns('bitakora.base.views',
     url(r'^(?P<slug>[\-\d\w]+)/my-posts$', 'my_posts', name='my_posts'),
     url(r'^(?P<slug>[\-\d\w]+)/my_comments$', 'my_comments', name='my_comments'),
     url(r'^(?P<blogslug>[\-\d\w]+)/add-article$','add_article', name='add_article'),
-    url(r'^(?P<blogslug>[\-\d\w]+)/rss$', BlogEntriesFeed(), name="blog_rss"),
+    #url(r'^(?P<blogslug>[\-\d\w]+)/rss$', BlogEntriesFeed(), name="blog_rss"),
+    url(r'^(?P<blogslug>[\-\d\w]+)/rss$', RedirectView.as_view(pattern_name='blog_rss')),
+    url(r'^(?P<blogslug>[\-\d\w]+)/feed.xml$', BlogEntriesFeed(), name="blog_rss"),
     url(r'^(?P<blogslug>[\-\d\w]+)/(?P<slug>[\-\d\w]+)$','article', name='article'),
     url(r'^(?P<blogslug>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/edit-article$','edit_article', name='edit_article'),
     url(r'^(?P<blogslug>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/delete-article$','delete_article', name='delete_article'),
